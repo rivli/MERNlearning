6757771896907f095cf4a56deda60f4173e3bc1c
ilvir_zv <zakilvir@gmail.com>
2019-04-15 14:18:30 +0500

    Post creating rout ready

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Post Model
+const Post = require("../../models/Post");
+
+// Load Validation
+const validatePostInput = require("../../validation/post");
 
 // @route  GET api/posts/test
 // @desc   Tests post route
 // @access Public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route  POST api/posts
+// @desc   Create Post route
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //Check Validation
+    if (!isValid) {
+      // if any errors, send 400 status with errors
+      res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      id: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
---
+++ validation/post.js
@@ -0,0 +1,21 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  if (!Validator.isLength(data.text, { min: 3, max: 3000 })) {
+    errors.text = "Text field must be between 3 and 3000 characters";
+  }
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Text field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
